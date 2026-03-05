74cf0784aa83b32e7cb01bb276a4946fc6fe9c26
Guts <nikaxaratishvili13@gmail.com>
2026-03-05 14:43:53 +0400

    initial comit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+function App() {
+
+
+  return (
+    <>
+    <div className="Bbb">
+      <h1>Name</h1>
+      <h1>Surname</h1>
+      <h1>Age</h1>
+      <h1>Hobby</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
