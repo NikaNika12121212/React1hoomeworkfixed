372bcb3ec7fb966bdcc51f2446915583d32d14e3
Guts <nikaxaratishvili13@gmail.com>
2026-03-08 11:48:00 +0400

    Update App.css styles for p tags

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ function App() {
 
 
   return (
-    <>
     <div className="Bbb">
       <h1>Name</h1>
+      <p>Nikoloz</p>
       <h1>Surname</h1>
+      <p>Kharatishvili</p>
       <h1>Age</h1>
+      <p>14</p>
       <h1>Hobby</h1>
+      <p>Riding Bicycle</p>
       </div>
-    </>
   )
 }
 
